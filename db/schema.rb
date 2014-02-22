27bc0dbeea09eb49e92caa98cc691ffbf7c53d61
Franco <fnc314@gmail.com>
2014-02-22 15:06:28 -0800

    Added Server_ID column to Tables for association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140222223117) do
+ActiveRecord::Schema.define(version: 20140222230452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,9 +62,11 @@ ActiveRecord::Schema.define(version: 20140222223117) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "restaurant_id"
+    t.integer  "server_id"
   end
 
   add_index "tables", ["restaurant_id"], name: "index_tables_on_restaurant_id", using: :btree
+  add_index "tables", ["server_id"], name: "index_tables_on_server_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
