6b48a6cc6daa54a775d3e7c05c29e092c71f3f5b
Franco <fnc314@gmail.com>
2014-02-21 17:35:00 -0800

    fixed model relationships fully

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ActiveRecord::Base
+  has_many :reservations
+  has_many :tables
+  has_many :servers
+  # admin?
 end
--- app/models/table.rb
+++ app/models/table.rb
@@ -1,2 +1,5 @@
 class Table < ActiveRecord::Base
+  has_many :reservations #are we sure about this?
+  belongs_to :server #questioned off 
+  belongs_to :restaurant
 end
