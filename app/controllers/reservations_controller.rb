67f29ba9bee4b5895a39188a99613dc555d5c880
Franco <fnc314@gmail.com>
2014-02-22 18:29:27 -0800

    touching-up the model relationships

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,13 @@
 class ReservationsController < ApplicationController
   
+  require ReservationsHelper
+
   def index
     @reservations = Reservation.all
+    respond_to do |f|
+      f.html
+      f.json { render json: @reservations, only: [:date, :time_begin, :party_size] }
+    end
   end
 
   def new
@@ -9,9 +15,18 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    new_res = params.require(:reservation).permit(:date, :time_begin, :party_size)
-    Reservation.create(new_res)
-    time_end = new_res[:time_begin]
+    new_res_params = params.require(:reservation).permit(:date, :time_begin, :party_size)
+    new_res = Reservation.create(new_res_params)
+
+    # Use method from ReservatoinsHelper to determine
+    # the end of a reservation.  Set that value to new instance of Reservation
+    time_end = end_time_calculator(:time_begin)
+    new_res.update_attributes(time_end: time_end)
+    @new_reservation = Reservation.last
+    respond_to do |f|
+      f.html
+      f.json { render json: @new_reservation, only: [:date, :time_begin, :party_size] }
+    end
   end
 
   def edit
--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,2 +1,10 @@
 module ReservationsHelper
+
+  # The method that adds 1.5 hrs to the begin time
+  # for a reservation and then returns the end_time
+  def end_time_calculator(begin_time)
+    end_time = begin_time + 5400
+    return end_time
+  end
+
 end
