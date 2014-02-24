de92123f31dfa6846e1f3fff8e4365146b4e2a03
Franco <fnc314@gmail.com>
2014-02-24 15:05:53 -0800

    formatted time entry for new reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -15,20 +15,20 @@ class ReservationsController < ApplicationController
     @times = []
     hours = [18, 19, 20, 21]
     hours.each do |x|
-      @times << l(Time.new(Time.now.year, Time.now.month, Time.now.day, hour = x), format: :default)
-      @times << l(Time.new(Time.now.year, Time.now.month, Time.now.day, hour = x, min = 30), format: :default)
+      @times << Time.new(Time.now.year, Time.now.month, Time.now.day, hour = x)
+      @times << Time.new(Time.now.year, Time.now.month, Time.now.day, hour = x, min = 30)
     end
   end
 
   def create
-    new_res_params = params.require(:reservation).permit(:date, :time_begin, :party_size)
-    new_res_params[:time_begin] = l(:time_begin, format: :default)
-    new_res = Reservation.create(new_res_params)
+
+
+    new_res = Reservation.create(permit_with_time_formatting)
 
     # Use method from ReservatoinsHelper to determine
     # the end of a reservation.  Set that value to new instance of Reservation
     time_end = end_time_calculator(new_res[:time_begin])
-    new_res.update_attributes(time_end: l(time_end, format: :default))
+    new_res.update_attributes(time_end: time_end)
     @new_reservation = Reservation.last
     respond_to do |f|
       f.html
@@ -44,5 +44,13 @@ class ReservationsController < ApplicationController
 
   def delete
   end
+
+  private
+
+    def permit_with_time_formatting    
+      new_params = params.require(:reservation).permit(:date, :time_begin, :party_size)
+      new_params[:time_begin] = new_params[:time_begin].to_time
+      new_params
+    end
 
 end
