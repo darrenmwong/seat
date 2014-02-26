f4184ccfc8d95b378a51f5c44b7bc97ca113f737
Franco <fnc314@gmail.com>
2014-02-25 21:23:33 -0800

    removed erroneous ',format: :default' from reservations_controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -29,7 +29,7 @@ class ReservationsController < ApplicationController
     
     # Create new DateTime object with each piece of date hash in params hash
     new_date[:begin] = DateTime.new(new_date["begin(1i)"].to_i, new_date["begin(2i)"].to_i, new_date["begin(3i)"].to_i, new_date["begin(4i)"].to_i, new_date["begin(5i)"].to_i)
-    new_params = { party_size: new_party[:party_size], begin: new_date[:begin], format: :default }
+    new_params = { party_size: new_party[:party_size], begin: new_date[:begin] }
     new_res = @user.reservations.create(new_params)
 
     # Use method from ReservatoinsHelper to determine
