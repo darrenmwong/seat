142a4ed896c320042b653eeb5c2965e580499db4
Franco <fnc314@gmail.com>
2014-02-26 20:16:46 -0800

    more work on Admin/Tables view and Admin/reservation/edit form

--- app/admin/reservations.rb
+++ app/admin/reservations.rb
@@ -1,18 +1,33 @@
 ActiveAdmin.register Reservation do
 
+  form do |f|
+    f.inputs "Label" do
+      f.input :party_size
+      f.input :begin
+      f.input :tables
+      f.input :server
 
-   index do
-    column(:when) { |res| l(res.begin.to_date, format: :default) }
-    column(:time) { |res| (res.begin.to_time + 8.hours).strftime("%l:%M %p") }
+      # etc
+    end
+    f.actions
+  end
+
+  filter :server
+  filter :party_size
+  filter :tables
+
+  index do
+    column(:date) { |res| l(res.begin.to_date, format: :default) }
+    column(:when) { |res| (res.begin.to_time + 8.hours).strftime("%l:%M %p") }
     column "Party Size", :party_size
     column(:server_id) { |res| Server.find(res.server_id).name }
-    default_actions
-   
+    column(:tables) { |res| res.tables.all }
+    default_actions 
   end
 
   controller do
     def permitted_params
-      params.permit reservation: [ :begin, :end, :party_size, :restaurant_id, :server_id, :tables, :image_file_size ]
+      params.permit reservation: [ :begin, :end, :party_size, :server_id, :tables, :image_file_size ]
     end
   end
 
